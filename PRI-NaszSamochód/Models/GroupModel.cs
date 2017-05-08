123eb8db1dfa2f923683251f5932b13d7185ea27
Michał Sokół <micsok@st.amu.edu.pl>
2017-05-08 09:33:27 +0200

    Added group model

---
+++ PRI-NaszSamochód/Models/GroupModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PRI_NaszSamochód.Models
+{
+    public class GroupModel
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string GroupName { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string GroupTheme { get; set; }
+
+        public virtual ICollection<PostModel> Posts { get; set; }
+
+        public virtual ICollection<ApplicationUser> Administrators { get; set; }
+
+        public virtual ICollection<ApplicationUser> Memebers { get; set; }
+    }
+}
