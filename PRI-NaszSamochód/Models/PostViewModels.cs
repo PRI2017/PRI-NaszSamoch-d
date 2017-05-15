26b370f3f20ec825a7dc5f276920dcb4203dad89
Mateusz Dajerling <mateusz.dajerling@gmail.com>
2017-05-15 11:30:09 +0200

    Add Creatorphoto to post model

--- PRI-NaszSamochód/Models/PostViewModels.cs
+++ PRI-NaszSamochód/Models/PostViewModels.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Ninject.Infrastructure.Language;
 
 namespace PRI_NaszSamochód.Models
 {
     public class PostViewModel
     {
+        public ApplicationUser Creator { get; set; }
+        public String CreatorPhoto { get; set; }
         public string Text { get; set; }
         public List<CommentViewModel> Comments { get; set; }
         public List<LikeModel> Likes { get; set; }
 
         public PostViewModel(PostModel post)
         {
+            Creator = post.Creator;
             Text = post.Text;
-            foreach(var i in post.Comments)
-            {
-                Comments.Add(new CommentViewModel(i));
-            }
+            post.Comments.Map
+                (comment => Comments.Add(new CommentViewModel(comment)));
             Likes = post.Likes.ToList();
+            CreatorPhoto = "/ProfPhoto?userId=" + Creator.Id;
         }
     }
 
