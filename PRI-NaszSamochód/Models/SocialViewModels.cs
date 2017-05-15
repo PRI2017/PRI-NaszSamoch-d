3c99be01477bb524a2edb23ae10fe056315dc489
Michał Sokół <micsok@st.amu.edu.pl>
2017-05-15 11:22:13 +0200

    Group controller

--- PRI-NaszSamochód/Models/SocialViewModels.cs
+++ PRI-NaszSamochód/Models/SocialViewModels.cs
@@ -5,6 +5,29 @@ using System.Web;
 
 namespace PRI_NaszSamochód.Models
 {
+    public class GroupViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Theme { get; set; }
+        public List<AdministratorsModel> Admins { get; set; }
+        public List<MembersModel> Members { get; set; }
+        public List<PostViewModel> LatestPosts { get; set; }
+
+        public GroupViewModel(GroupModel group)
+        {
+            Name = group.GroupName;
+            Description = group.Description;
+            Theme = group.GroupTheme;
+            Admins = group.Administrators;
+            Members = group.Memebers;
+            foreach (var i in group.Posts.OrderByDescending(x=>x.Added).Take(20))
+            {
+                LatestPosts.Add(new PostViewModel(i));
+            }
+        }
+    }
+
     public class PostViewModel
     {
         public string Text { get; set; }
