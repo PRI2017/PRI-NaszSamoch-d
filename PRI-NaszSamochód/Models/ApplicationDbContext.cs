138e98bdf6de222ed13ac2a505062080513fc78b
Michał Sokół <micsok@st.amu.edu.pl>
2017-05-17 15:47:52 +0200

    Should Work

--- PRI-NaszSamochód/Models/ApplicationDbContext.cs
+++ PRI-NaszSamochód/Models/ApplicationDbContext.cs
@@ -18,8 +18,8 @@ namespace PRI_NaszSamochód.Models
         public DbSet<PostModel> Posts { get; set; }
         public DbSet<FriendModel> Friends { get; set; }
         public DbSet<GroupModel> Groups { get; set; }
-        public DbSet<VehicleModel> Vehicles { get; set; }
-        public DbSet<RouteModel> Routes { get; set; }
+        //public DbSet<VehicleModel> Vehicles { get; set; }
+        //public DbSet<RouteModel> Routes { get; set; }
 
     }
 }
