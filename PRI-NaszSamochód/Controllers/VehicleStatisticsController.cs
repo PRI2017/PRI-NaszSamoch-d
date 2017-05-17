c950d0d55a70c69805a7d623a75e8e3ee24ba4b2
Michał Sokół <micsok@st.amu.edu.pl>
2017-05-17 15:36:29 +0200

    Revert "Revert "sdas""
    This reverts commit d5b22638e82359fb4972e21d743facf391037187.

--- PRI-NaszSamochód/Controllers/VehicleStatisticsController.cs
+++ PRI-NaszSamochód/Controllers/VehicleStatisticsController.cs
@@ -23,19 +23,96 @@ namespace PRI_NaszSamochód.Controllers
             return RedirectToAction("VehicleStatisticsHeader");
         }
 
-        [HttpGet]
-        public ActionResult VehicleStatisticsHeader(int? vehicleId)
+        public ActionResult VehicleStatisticsHeader(int? Id)
         {
-            if(vehicleId == null)
+            if (Id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            VehicleStatisticsModel model = _context.Cars.Find(vehicleId).Statistics;
+            VehicleStatisticsModel model = _context.Vehicles.Find(Id).Statistics;
             if (model == null)
             {
-                model = _context.Motorcycles.Find(vehicleId).Statistics;
+                return HttpNotFound();
             }
             return View(model);
+        }
+
+        public ActionResult VehicleStatisticsContent()
+        {
+            return View();
+        }
+
+        // POST: Groups/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Key, KilometersDriven, FuelUsed, MaxVelocity, RecordStartTime, RecordEndTime")]VehicleStatisticsModel model, int vehicleId)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Vehicles.Find(vehicleId).Statistics = model;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(_context.Vehicles.Find(vehicleId).Statistics);
+        }
+
+        // GET: Groups/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GroupModel group = _context.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // POST: Groups/Edit/5
+        [System.Web.Mvc.HttpPut]
+        public ActionResult Edit([Bind(Include = "Key, KilometersDriven, FuelUsed, MaxVelocity, RecordStartTime, RecordEndTime")]VehicleStatisticsModel model, int vehicleId)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Vehicles.Find(vehicleId).Statistics = model;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        // GET: Groups/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GroupModel group = _context.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // POST: Groups/Delete/5
+        [System.Web.Mvc.HttpDelete, System.Web.Mvc.ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int vehicleId)
+        {
+            VehicleModel model = _context.Vehicles.Find(vehicleId);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            if (!(model.Statistics == null))
+            {
+                _context.Vehicles.Find(vehicleId).Statistics = null;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
