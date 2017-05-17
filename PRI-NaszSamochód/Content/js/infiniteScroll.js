47c12dcb418854c7b94672697013e6436bb4ee77
konikowska <katarzyna.konikowska@gmail.com>
2017-05-17 14:48:55 +0200

    repaired content of content XD, css, added scrolling mode - in progress

---
+++ PRI-NaszSamochód/Content/js/infiniteScroll.js
@@ -0,0 +1,14 @@
+﻿$('head').append($('<script>').attr('src', '/Content/node_modules/jquery/dist/jquery.min.js'));
+$.getScript("/Content/node_modules/jscroll/jquery.jscroll.js", function (data, textStatus, jqxhr) {
+    //console.log(data); // Data returned
+    //console.log(textStatus); // Success
+    //console.log(jqxhr.status); // 200
+    //console.log("Load was performed.");
+
+    $(document).ready(function () {
+        $('#scrollIt').jscroll({
+            autoTrigger: true,
+            loadingHtml: '<img src="/Content/src/loading.gif/" />',
+        });
+    });
+});
