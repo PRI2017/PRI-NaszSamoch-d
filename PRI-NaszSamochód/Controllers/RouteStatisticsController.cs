2998e264d0e2fb94c3958a4eb5eccd7cd44114d2
Michał Sokół <sokol.michal95@live.com>
2017-05-20 18:55:28 +0200

    Update RouteStatisticsController.cs

--- PRI-NaszSamochód/Controllers/RouteStatisticsController.cs
+++ PRI-NaszSamochód/Controllers/RouteStatisticsController.cs
@@ -1,42 +1,45 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net;
-//using System.Web;
-//using System.Web.Mvc;
-//using PRI_NaszSamochód.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PRI_NaszSamochód.Models;
 
-//namespace PRI_NaszSamochód.Controllers
-//{
-//    [Authorize]
-//    public class RouteStatisticsController : Controller
-//    {
-//        private readonly ApplicationDbContext _context;
+namespace PRI_NaszSamochód.Controllers
+{
+    [Authorize]
+    public class RouteStatisticsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RouteStatisticsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: RouteStatistics
+        [ActionName("GetRouteStatsHeader")]
+        public ActionResult Index()
+        {
+            return RedirectToAction("RouteStatisticsHeader");
+        }
 
-//        public RouteStatisticsController(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
-
-//        // GET: RouteStatistics
-//        public ActionResult Index()
-//        {
-//            return RedirectToAction("RouteStatisticsHeader");
-//        }
-
-
-//        public ActionResult RouteStatisticsHeader(int? routeId)
-//        {
-//            if(routeId == null)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-//            }
-//            RouteStatisticsModel model = _context.Routes.Find(routeId).Statistics;
-//            if(model == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(model);
-//        }
-//    }
-//}
+        public ActionResult RouteStatisticsHeader(int? routeId)
+        {
+            if(routeId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RouteStatisticsModel model = _context.Routes.Find(routeId).Statistics;
+            if(model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+        
+        public ActionResult RouteStatisticsContent()
+        {
+            return View();
+        }
+    }
+}
