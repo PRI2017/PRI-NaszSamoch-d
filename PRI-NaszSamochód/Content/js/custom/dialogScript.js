33e221d4c40e047eb7f2e80d83f23ddd51d61276
konikowska <katarzyna.konikowska@gmail.com>
2017-05-24 15:45:10 +0200

    reform structure in js files and script section in html

---
+++ PRI-NaszSamochód/Content/js/custom/dialogScript.js
@@ -0,0 +1,16 @@
+﻿
+    document.getElementById('userProfilePhoto').addEventListener("click", showDialog);
+function showDialog() {
+    $('head').append($('<link>').attr('href', 'https://rawgit.com/enyo/dropzone/master/dist/dropzone.css')); 
+    $.getScript("/Content/js/lib/dropzone.js",
+        function (data, textStatus, jqxhr) {
+            $("#dialog").dialog().addClass("dropzone").dropzone({ url: "/ProfPhoto/Upload" });
+            document.getElementById("my-awesome-dropzone").style.display = "flex";
+            console.log("Created dropzone");
+        }
+    );
+   
+        //console.log("Drop zone script!!!");
+   
+    
+};
