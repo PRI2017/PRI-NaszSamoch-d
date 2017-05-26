215329c73bb60f4130de75047cebe24dc16a2931
Mateusz Dajerling <mateusz.dajerling@gmail.com>
2017-05-26 20:17:23 +0200

    gallery commit  to merge

---
+++ PRI-NaszSamochód/Models/UserGallery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+
+namespace PRI_NaszSamochód.Models
+{
+    public class UserGalleryModel
+
+    {
+
+        public int Id { get; set; }
+        public ApplicationUser Owner { get; set; }
+        public virtual List<PhotoModel> PhotosList { get; set; } 
+
+
+    }
+}
