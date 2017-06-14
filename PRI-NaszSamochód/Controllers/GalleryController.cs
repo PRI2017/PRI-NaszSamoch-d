8f51c6e2f5d5c024a0133665156b0ff67a0d4138
Michał Sokół <micsok@st.amu.edu.pl>
2017-06-14 19:16:32 +0200

    Remodeled user galleries

--- PRI-NaszSamochód/Controllers/GalleryController.cs
+++ PRI-NaszSamochód/Controllers/GalleryController.cs
@@ -7,12 +7,28 @@ using AttributeRouting.Web.Mvc;
 using PRI_NaszSamochód.Models;
 using System.Web.Http;
 using Microsoft.AspNet.Identity;
+using System.Net;
 
 public class GalleryController : Controller
 {
-    [GET("Gallery/{galleryId}")]
-    public ActionResult UserGallery(int galleryId)
+
+    [GET("Gallery/")]
+    public ActionResult GetAll()
     {
+        var db = ApplicationDbContext.Create();
+        string id = User.Identity.GetUserId();
+        var a = db.Galleries.Where(x => x.Owner.Id == id).ToList();
+        UserGalleryViewModel all = new UserGalleryViewModel(a);
+        return View(all);
+    }
+
+    [GET("Gallery/{galleryId}")]
+    public ActionResult UserGallery(int? galleryId)
+    {
+        if (galleryId == null)
+        {
+            return RedirectToAction("GetAll");
+        }
         ApplicationDbContext db = ApplicationDbContext.Create();
         UserGalleryModel gallery = db.Galleries.Single(x => x.Id == galleryId);
         return View(gallery);
@@ -26,10 +42,23 @@ public class GalleryController : Controller
     {
         var db = ApplicationDbContext.Create();
         string userId = User.Identity.GetUserId();
-        ApplicationUser owner = db.Users.Where(x=>x.Id == userId).FirstOrDefault();
+        ApplicationUser owner = db.Users.Where(x => x.Id == userId).FirstOrDefault();
         UserGalleryModel model = new UserGalleryModel(name, owner);
         db.Galleries.Add(model);
         db.SaveChanges();
         return RedirectToAction("UserGallery", new { galleryId = model.Id });
+    }
+
+    public ActionResult Delete(int? id)
+    {
+        if (id == null)
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+        var db = ApplicationDbContext.Create();
+        UserGalleryModel entity = db.Galleries.Find(id);
+        db.Galleries.Remove(entity);
+        db.SaveChanges();
+        return RedirectToAction("GetAll");
     }
 }
